a76b427823c5dbea0297bc4dd856609f975fd3e9
Ayush-Singh-01 <82030021+Ayush-Singh-01@users.noreply.github.com>
2021-04-13 04:14:13 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Payment Gateway Integration</title>
+    <style>
+    #main{
+        color: #0000cc;
+        width: 400px;
+        height: 60px;
+        font-size: 25px;
+    }
+    </style>
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- off-canvas -->
+    <link href="css/mobile-menu.css" rel="stylesheet">
+    <!-- font-awesome -->
+    <link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div id="main-wrapper">
+<!-- Page Preloader -->
+<div id="preloader">
+    <div id="status">
+        <div class="status-mes"></div>
+    </div>
+</div>
+
+<div class="uc-mobile-menu-pusher">
+
+<div class="content-wrapper">
+<nav class="navbar m-menu navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">   
+            <a class="navbar-brand" href="home.php" id="main"><img src="img/log.png"></a>
+        </div>
+        <div class="collapse navbar-collapse" id="#navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right main-nav">
+                <li class="active"><a href="home.php" style="background-color: black;height: 40px;">HOME</a></li>
+                <li><a href="https://www.thesparksfoundationsingapore.org/programs/student-scholarship-program/" style="background-color: black;">PROGRAMS</a></li>
+                <li><a href="https://www.thesparksfoundationsingapore.org/about/vision-mission-and-values/" style="background-color: black;">ABOUT</a></li>
+                <li><a href="https://rzp.io/l/DocVkfO45k" style="background-color: black;">DONATE</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div id="x-corp-carousel" class="carousel slide hero-slide hidden-xs" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="img/hii.jpg" alt="Hero Slide" style="width: 2200px;">
+    </div>
+</div>
+<footer class="footer">
+    <div class="footer-widget-section">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-4 footer-block">
+                    <div class="footer-widget widget_text">
+                        <div class="footer-logo">
+                            <a href="https://www.thesparksfoundationsingapore.org/"><img src="img/logo.png" ></a>
+                        </div>
+                        <p>TSF Donation</p>
+                        <p>Made By:Ayush Kumar Singh</p>
+                        <p><a href="#">#GRIPAPR2021</a> Web Development & Designing Internship</p>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 footer-block">
+                    <div class="footer-widget widget_text">
+                        <h3>About Us</h3>
+
+                        <p>Education is a fundamental right for all children, but girls from impoverished, illiterate families are mostly denied this right in India. By the time they reach adolescence, 40% are out of school, kept at home doing household chores. Join Mission: Every Girl in School and support the education of thousands of girls. ₹600/month can make a huge difference to one girl's life.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 footer-block last">
+                    <div class="footer-widget widget_text">
+                        <h3>Contact Us Today</h3>
+                        <address>
+                            Call Us 666 777 888 OR 111 222 333<br>
+                            Send an Email on <a href="mailto:#">contact@tsfdonation.com</a><br>
+                            Visit Us 123 Fake Street- Blla 12358<br>
+                            Singapore<br>
+                        </address>
+
+                        <ul class="list-inline social-links">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</footer>
+</div>
+</div>
+</div>
+<!-- Script -->
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/mobile-menu.js"></script>
+<script src="js/flexSlider/jquery.flexslider-min.js"></script>
+<script src="js/scripts.js"></script>
+
+</body>
+</html>
